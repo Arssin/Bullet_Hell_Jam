3bed775a3bb48b66a68e647f76ee8c0a99dbfb72
Mateusz Binięda <mat.binieda@gmail.com>
2024-05-11 14:23:09 +0200

    Changin scenes functionality

--- scripts/doors.gd
+++ scripts/doors.gd
@@ -2,16 +2,28 @@ extends Node2D
 
 @onready var label: Label = %Label
 
+var name_doors : String
+var player_in_area = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 func update_doors_label(name):
-	label.text = 'TEST' + name
+	name_doors = 'TEST' + name
+	
 
+func _input(event):
+	if Input.is_action_just_pressed("player_action") && player_in_area:
+		GlobalManager.load_level("Level2")
+	
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is Player:
+		player_in_area = true
+		%Label.text = name_doors
+		%Label.show()
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body is Player:
+		player_in_area = false
+		%Label.hide()
