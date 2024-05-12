8561d48a745c211463083a9c9a83d2624f38c3f8
Mateusz Binięda <mat.binieda@gmail.com>
2024-05-12 21:50:35 +0200

    level 2 prepare and random run

--- scripts/Small_Runing_Mob.gd
+++ scripts/Small_Runing_Mob.gd
@@ -5,14 +5,63 @@ var isAlive: bool = true
 var speed: int = 20
 @onready var player: Node = get_node("../../Player")
 @onready var sprite = get_node("AnimatedSprite2D")
+@onready var agent: NavigationAgent2D = get_node("NavigationAgent2D")
+var move_direction: Vector2
+var home_position: Vector2
+
+func _ready() -> void:
+	home_position = self.global_position
+	if isAlive:
+		sprite.play("run")
+		new_wander_pos()
 
 func _physics_process(delta: float) -> void:
 	if isAlive:
-		var direction: Vector2 = (player.global_position - self.global_position).normalized()
-		velocity = speed * direction
-		move_and_slide()
+		var target_pos = agent.get_next_path_position()
+		var move_dir = global_position.direction_to(target_pos)
+		move_dir = move_dir.normalized()
+		#var direction: Vector2 = (player.global_position - self.global_position).normalized()
+		#velocity = speed * direction
+		if agent.is_navigation_finished():
+			move_dir = Vector2.ZERO
 		
-		if direction.x < 0:
-			sprite.flip_h = false
-		else:
-			sprite.flip_h = true
+		velocity.x = move_dir.x * speed
+		velocity.y = move_dir.y * speed
+		move_and_slide()
+		#if direction.x < 0:
+			#sprite.flip_h = false
+		#else:
+			#sprite.flip_h = true
+
+
+func _on_navigation_agent_2d_target_reached() -> void:
+	print('reacz')
+	var wait_time = randf_range(1, 4)
+	await get_tree().create_timer(wait_time).timeout
+	
+	new_wander_pos()
+
+	
+func new_wander_pos():
+	var pos = home_position + random_offset() * randf_range(0, 200)
+	print('POZYCJA', pos)
+	print('pozycja globalna', global_position)
+	move_to_position(pos)
+	
+func move_to_position (to_position: Vector2, adjust_pos: bool = true):
+	print(to_position)
+	if not agent:
+		agent = get_node("NavigationAgent2D")
+	
+	#if adjust_pos:
+		#var map = get_world_2d().navigation_map
+		#NavigationSer
+		#var adjusted_pos = NavigationServer2D.map_get_closest_point(map, to_position)
+		#print('ADJK', adjusted_pos)
+		#agent.target_position = adjusted_pos
+	#else:
+	agent.target_position = to_position
+		
+func random_offset () -> Vector2:
+	var offset = Vector2(randf_range(-1, 1),randf_range(-1, 1))
+	return offset.normalized()
--- scripts/portal.gd
+++ scripts/portal.gd
@@ -8,41 +8,45 @@ var mob_pool: Array = []
 @onready var timer: Timer = get_node("Timer")
 
 func _ready() -> void:
-	timer.wait_time = randi_range(1,3)
+	timer.wait_time = randi_range(3,5)
 	animated_sprite_2d.play("portal_resp")
-	for i in range(pool_size):
-		var mobTemp: Node = mob_scene.instantiate()
-		mobTemp.hide()
-		mob_pool.append(mobTemp)
-		add_child(mobTemp)
+	#for i in range(pool_size):
+		#var mobTemp: Node = mob_scene.instantiate()
+		#mobTemp.hide()
+		#mob_pool.append(mobTemp)
+		#add_child(mobTemp)
 
 
 
 func _on_animated_sprite_2d_animation_finished() -> void:
 	animated_sprite_2d.play("portal_standing")
 	
-func get_mob()-> Node:
-	for mob in mob_pool:
-		if not mob.visible:
-			return mob
-			
-	var new_mob: Node = mob_scene.instantiate()
-	new_mob.hide()
-	mob_pool.append(new_mob)
-	add_child(new_mob)
-	return new_mob
-	
-func reset_mob(mob:Node) -> void:
-	mob.position = self.position
-	mob.get_node("CollisionShape2D").disabled = false
-	mob.isAlive = false
-	mob.hide()
-
-func _respawn_mob():
-	pass
+#func get_mob()-> Node:
+	#for mob in mob_pool:
+		#if not mob.visible:
+			#return mob
+			#
+	#var new_mob: Node = mob_scene.instantiate()
+	#new_mob.hide()
+	#mob_pool.append(new_mob)
+	#add_child(new_mob)
+	#return new_mob
+	#
+#func reset_mob(mob:Node) -> void:
+	#mob.position = self.position
+	#mob.get_node("CollisionShape2D").disabled = false
+	#mob.isAlive = false
+	#mob.hide()
+#
+#func _respawn_mob():
+	#pass
 
 
 func _on_timer_timeout() -> void:
-	var mobTemp: Node = get_mob()
-	mobTemp.global_position = self.global_position
-	mobTemp.show()
+	var new_mob: Node = mob_scene.instantiate()
+	#new_mob.hide()
+	#mob_pool.append(new_mob)
+	add_child(new_mob)
+	#var mobTemp: Node = get_mob()
+	#mobTemp.global_position = self.global_position
+	#mobTemp.show()
