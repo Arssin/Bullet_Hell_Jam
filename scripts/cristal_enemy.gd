df091275ed2d73730f8392ed94a362f7ef89c392
Mateusz Binięda <mat.binieda@gmail.com>
2024-05-13 00:48:44 +0200

    spawning attacks

--- scripts/cristal_enemy.gd
+++ scripts/cristal_enemy.gd
@@ -6,16 +6,25 @@ extends CharacterBody2D
 var previous_global_position = Vector2.ZERO
 
 @onready var player = get_node('../../Player')
+@onready var attack: Timer = $Attack
+
 
 func _ready() -> void:
+	attack.wait_time = randf_range(0.7,1.5)
 	$AnimatedSprite2D.play("cristal")
 	previous_global_position = global_position
 
 func _physics_process(delta: float) -> void:
-	print(player.global_position)
 	move_on_path.progress += movement_speed
 	position = move_on_path.position
 	var current_global_position = global_position
 	previous_global_position = current_global_position
 	
-func 
+func attack_spawn():
+	$PlayerMarker.global_position = player.global_position
+	Spawning.spawn({"position": global_position, "rotation": 0}, "cristal", "two")
+
+
+func _on_attack_timeout() -> void:
+	print('go')
+	attack_spawn()
