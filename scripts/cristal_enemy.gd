d47b7ac7070290f01160ee0dbe123aae712e4210
Mateusz Binięda <mat.binieda@gmail.com>
2024-05-13 16:59:47 +0200

    prepare lvl 4

--- scripts/cristal_enemy.gd
+++ scripts/cristal_enemy.gd
@@ -8,22 +8,28 @@ var previous_global_position = Vector2.ZERO
 @onready var player = get_node('../../Player')
 @onready var attack: Timer = $Attack
 
+@onready var static_lvl = get_parent().name == "Enemies4"
 
 func _ready() -> void:
+	print(static_lvl, get_parent())
 	attack.wait_time = randf_range(0.7,1.5)
 	$AnimatedSprite2D.play("cristal")
 	previous_global_position = global_position
 
 func _physics_process(delta: float) -> void:
-	move_on_path.progress += movement_speed
-	position = move_on_path.position
-	var current_global_position = global_position
-	previous_global_position = current_global_position
+	if !static_lvl:
+		move_on_path.progress += movement_speed
+		position = move_on_path.position
+		var current_global_position = global_position
+		previous_global_position = current_global_position
 	
 func attack_spawn():
-	if player:
+	if player && !static_lvl:
 		$PlayerMarker.global_position = player.global_position
 		Spawning.spawn({"position": global_position, "rotation": 0}, "cristal", "two")
+	
+	if player && static_lvl:
+		pass
 
 
 func _on_attack_timeout() -> void:
