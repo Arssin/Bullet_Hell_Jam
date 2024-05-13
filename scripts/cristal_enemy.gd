426bd76be39b7218be17112706c2f906ce1776b4
Mateusz Binięda <mat.binieda@gmail.com>
2024-05-13 17:20:27 +0200

    create mini totem

--- scripts/cristal_enemy.gd
+++ scripts/cristal_enemy.gd
@@ -22,14 +22,25 @@ func _physics_process(delta: float) -> void:
 		position = move_on_path.position
 		var current_global_position = global_position
 		previous_global_position = current_global_position
-	
+		
+var new_pattern: PatternLine = PatternLine.new()
+
 func attack_spawn():
 	if player && !static_lvl:
 		$PlayerMarker.global_position = player.global_position
 		Spawning.spawn({"position": global_position, "rotation": 0}, "cristal", "two")
+		
+	
 	
 	if player && static_lvl:
-		pass
+		new_pattern.offset = Vector2i(0,0)
+		new_pattern.bullet = "5"
+		new_pattern.nbr = 1
+		new_pattern.forced_lookat_mouse = false
+		new_pattern.forced_pattern_lookat = false
+		Spawning.new_pattern("cristal_2", new_pattern)
+	
+		Spawning.spawn({"position": global_position, "rotation": 0}, "cristal_2", "two")
 
 
 func _on_attack_timeout() -> void:
