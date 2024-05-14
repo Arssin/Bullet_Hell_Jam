6322769fe69c860c9eb7258acf58a76ba720abc1
Mateusz Binięda <mat.binieda@gmail.com>
2024-05-14 23:36:27 +0200

    mini totem prepare

---
+++ scripts/mini_totem.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+var max_health = 100
+var health = 100
+
+func _ready():
+	$HealthBar.max_value = max_health
+	$HealthBar.value = health
+	
+
+func get_hit(value):
+	health = health - (value + PlayerGlobals.additional_dmg)
+	$HealthBar.update_healthbar(health)
+	if health <= 0:
+		die()
+		
+		
+func die():
+	queue_free()
