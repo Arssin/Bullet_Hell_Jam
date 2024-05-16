56dd43558ebbfbf062325dc54ec12809f43fb5a5
Mateusz Binięda <mat.binieda@gmail.com>
2024-05-16 07:42:35 +0200

    lvl 7

--- scripts/bomb.gd
+++ scripts/bomb.gd
@@ -1,6 +1,25 @@
 extends CharacterBody2D
 
+@export var detonation_dmg = 10
+var player: Player
+var chase = true
+
+func _physics_process(delta: float) -> void:
+	if chase && player:
+		velocity = (player.global_position - position) * 10
 
 
 func _on_chase_area_body_entered(body: Node2D) -> void:
+	if body is Player:
+		player = body
+		chase = true
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
 	pass # Replace with function body.
+
+
+func _on_chase_area_body_exited(body: Node2D) -> void:
+	if body is Player:
+		player = null
+		chase = false
