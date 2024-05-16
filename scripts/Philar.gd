ca77f1cb97cc39914506c72a7862408315d6d4c7
Mateusz Binięda <mat.binieda@gmail.com>
2024-05-16 11:28:08 +0200

    lvl 1  upgrades

--- scripts/Philar.gd
+++ scripts/Philar.gd
@@ -1,8 +1,8 @@
 extends StaticBody2D
 
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-	pass # Replace with function body.
+	$AnimatedSprite2D.play("static")
 
 
