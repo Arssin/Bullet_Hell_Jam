688d8ae30089d946eb7a80fdd1bd905ea98a1264
Mateusz Binięda <mat.binieda@gmail.com>
2024-05-19 06:02:09 +0200

    main scene i init lvl info

--- scripts/PlayerGlobals.gd
+++ scripts/PlayerGlobals.gd
@@ -6,8 +6,8 @@ var max_speed = 180
 
 var player_max_health = 10
 var player_attack_speed = 200
-var player_move_speeds = 300
-var additional_dmgs = 100
+var player_move_speeds = 100
+var additional_dmgs = 10
 
 # attck 10 speed 100
 
